9786a0efb32bfde8092321ad32c237543abc7667
lishengyuan <38453126+lishengyuan@users.noreply.github.com>
2019-01-15 17:26:26 +0800

    Update Main.java

--- Arithmetic/src/Main.java
+++ Arithmetic/src/Main.java
@@ -3,5 +3,49 @@ import SortSet.QuickSort;
 public class Main {
 
     public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		// args[0] 税前工资 例如：15000
+//		// args[1]社保+公积金 例如：3000
+//		// args[2]专项扣除 例如：2500
+//		if (args == null || args.length < 1) {
+//			args = new String[] { "18215", "3575", "2500" };
+//		}
+//
+//		float gongzi = Float.parseFloat(args[0]);
+//		float wuxiangyijin = Float.parseFloat(args[1]);
+//		float zhuangxiang = Float.parseFloat(args[2]);
+//		float gesuiSum = 0;
+//		float totalGesui;
+//		for (int i = 1; i <= 12; i++) {
+//			float thisjiaosuie = gongzi * i - (wuxiangyijin * i) - (5000 * i) - (zhuangxiang * i);
+//			float suilv = 0.03f;
+//			float kouchu = 0f;
+//			if (thisjiaosuie <= 36000) {
+//				suilv = 0.03f;
+//				kouchu = 0f;
+//			} else if (thisjiaosuie <= 144000) {
+//				suilv = 0.1f;
+//				kouchu = 2520f;
+//			} else if (thisjiaosuie <= 300000) {
+//				suilv = 0.2f;
+//				kouchu = 16920f;
+//			} else if (thisjiaosuie <= 420000) {
+//				suilv = 0.25f;
+//				kouchu = 31920f;
+//			} else if (thisjiaosuie <= 660000) {
+//				suilv = 0.3f;
+//				kouchu = 52920f;
+//			} else if (thisjiaosuie <= 960000) {
+//				suilv = 0.35f;
+//				kouchu = 85920f;
+//			} else {
+//				suilv = 0.45f;
+//				kouchu = 181920f;
+//			}
+//			float thisgesui = thisjiaosuie * suilv - kouchu - gesuiSum;
+//			gesuiSum += thisgesui;
+//			System.out.println(i + "-: " + thisgesui);
+//		}
+//		System.out.println("total: " + gesuiSum);
     }
 }
